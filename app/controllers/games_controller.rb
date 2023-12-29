81370bf9251a84626fc4200d6a1cc535e7ccdb80
stevenungaro <144843553+stevenungaro@users.noreply.github.com>
2023-12-29 11:34:02 -0600

    edited games create controller to take in a bgg id

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,13 +13,39 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = Game.new(
-      game_name: params["game_name"],
-      image_url: params["image_url"],
-      release_year: params["release_year"],
-      description: params["description"],
-      bgg_id: params["bgg_id"],
-    )
+    the_id = params[:bgg_id]
+
+    gamerequest = HTTP.get("https://boardgamegeek.com/xmlapi2/thing?id=#{the_id}&type=boardgame,boardgameexpansion")
+    response = Hash.from_xml(gamerequest).to_json
+    data = JSON.parse(response)
+    if data && data["items"] && data["items"]["item"]
+      is_data = true
+    else
+      is_data = false
+    end
+
+    if gamerequest.code == 200 && is_data
+      if data["items"]["item"]["name"][0]
+        entry_game_name = data["items"]["item"]["name"][0]["value"]
+      else
+        entry_game_name = data["items"]["item"]["name"]["value"]
+      end
+
+      entry_game_type = data["items"]["item"]["type"]
+      entry_image_url = data["items"]["item"]["image"]
+      entry_release_year = data["items"]["item"]["yearpublished"]["value"]
+      entry_description = data["items"]["item"]["description"]
+      entry_bgg_id = data["items"]["item"]["id"]
+
+      @game = Game.new(
+        game_name: entry_game_name,
+        game_type: entry_game_type, image_url: entry_image_url,
+        release_year: entry_release_year,
+        description: entry_description,
+        bgg_id: entry_bgg_id,
+      )
+    end
+
     @game.save
     if @game.valid?
       render :show
