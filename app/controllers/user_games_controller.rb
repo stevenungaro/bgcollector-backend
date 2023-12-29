7aec24d645656c98af4a9ee34f7d4f03bf7d16a5
stevenungaro <144843553+stevenungaro@users.noreply.github.com>
2023-12-29 10:50:44 -0600

    fixed controllers

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,5 +1,5 @@
 class UserGamesController < ApplicationController
-  before_action :authenticate_user, except: [:index, :show]
+  before_action :authenticate_user
 
   def index
     @user_games = current_user.user_games
